22e4bd1c3d3ef93be752286828ddb201228429db
Vaishakh <vaishakhp1998@gmail.com>
2021-11-22 20:54:19 +0530

    add redux store and carsReducer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 
+import { useSelector } from 'react-redux'
+
+
+
 function Home() {
+
+    const {cars} = useSelector(state=>state.carsReducer)
     return (
         <DefaultLayout>
             <h1>Homeapage</h1>
+            <h1>length of cars array is {cars.length}</h1>
         </DefaultLayout>
     )
 }
---
+++ client/src/redux/reducers/carsReducer.js
@@ -0,0 +1,13 @@
+const initialData = {
+    cars:['tata']
+}
+
+
+export const carsReducer = (state=initialData,action) => {
+    switch(action.type)
+    {
+        default: return state
+    }
+}
+
+
