bdffa9b654d884969c3c3cbd58c908d2d01ecb77
Vaishakh <vaishakhp1998@gmail.com>
2021-11-22 20:24:02 +0530

    getall cars api from mongo

--- server/server.js
+++ server/server.js
@@ -4,5 +4,8 @@ const port = 5001
 
 const dbConnection = require('./db')
 
+
+app.use('/api/cars',require('./routes/carsRoute'))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Node server started on${port}!`))
