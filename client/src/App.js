75923016068bb4a93f366d35852117985735046a
Vaishakh <vaishakhp1998@gmail.com>
2021-11-23 09:48:48 +0530

    homepage, display cars along with image, booking button and styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
+import 'antd/dist/antd.css'
+
 function App() {
     return (
         <div className="App">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,9 @@ import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 
 
+import { Button,Row,Col } from 'antd'
+
+
 
 function Home() {
 
@@ -19,8 +22,43 @@ function Home() {
 
     return (
         <DefaultLayout>
-            <h1>Homeapage</h1>
-            <h1>length of cars array is {cars.length}</h1>
+        <Row justify='center' gutter={16} className='mt-5'>
+
+        {cars.map(car=>{
+
+            return <Col lg={5} sm={24} xs={24}>
+           <div className='car p-2 bs1 mt-4'>
+           <img src={car.image} className='car-img'/>
+           <div className='car-content d-flex align-items-center justify-content-between'>
+
+           <div>
+
+           <p>{car.name}</p>
+           <p>{car.rentPerHour} Rent per hour</p>
+           
+           </div>
+
+           <div>
+           <button className='btn-1 mr-2'>Book Now</button>
+           
+           
+           </div>
+           
+           
+           </div>
+
+           
+           </div>
+
+
+
+            </Col>
+        })}
+        
+        
+        </Row>
+        
+            
         </DefaultLayout>
     )
 }
